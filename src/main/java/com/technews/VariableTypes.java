f31e80f85f9e70a3ffa9b92f6989756f0e290508
AlessandroB96 <abelaj7196@gmail.com>
2022-06-04 15:44:12 -0400

    variable types class

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,10 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name= "Alessandro";
+        int age= 25;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
