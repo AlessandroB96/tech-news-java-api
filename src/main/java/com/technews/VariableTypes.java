5a8cb38b5fa40d7bdb0d3d18bb4e6fc892ab34dd
AlessandroB96 <abelaj7196@gmail.com>
2022-06-05 15:19:46 -0400

    overriding default functionality

--- src/main/java/com/technews/VariableTypes.java
+++ src/main/java/com/technews/VariableTypes.java
@@ -4,7 +4,7 @@ public class VariableTypes {
     public static void main(String[] args) {
         String name= "Alessandro";
         int age= 25;
-        System.out.println(name);
+        System.out.printf(name);
         System.out.println(age);
     }
 }
